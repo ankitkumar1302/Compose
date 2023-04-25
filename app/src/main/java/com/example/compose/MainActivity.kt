13d6074d0938dcdc3f54c7e5d35f3f3d3244d3be
ankit <you@example.com>
2023-04-25 18:33:20 +0530

    Column tutorial

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
@@ -25,35 +30,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column(
+                        // Used for vertical arrangement
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CustomItem(weight = 3f, MaterialTheme.colorScheme.primary)
+                        CustomItem(weight = 1f)
+                        // Default value has been assigned... in the CustomItem
+                    }
                 }
             }
         }
     }
 }
 
+// Created a customItem function
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.secondary) {
+    // To use the weight function we need to call a extension function (ColumnScope).
+    Surface(
+        // Don't use column here because it combine the different surface to each other.
+        // We Don't use column as a parent of surface
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeTheme {
-
         Column(
-            modifier = Modifier.fillMaxSize(),
+            // Used for vertical arrangement
+            modifier = Modifier.height(500.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
         ) {
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .weight(3f),
-                color = MaterialTheme.colorScheme.secondary
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .weight(1f),
-                color = MaterialTheme.colorScheme.primary
-            ) {}
+            CustomItem(weight = 3f, MaterialTheme.colorScheme.primary)
+            CustomItem(weight = 1f)
+            // Default value has been assigned... in the CustomItem
         }
 
     }
